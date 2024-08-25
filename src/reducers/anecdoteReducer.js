05ea5d77dcc628cad0530510ef8186b6f8c3e311
Michal Puchy <puchy.michal@gmail.com>
2024-08-25 14:03:29 +0200

    Revert "another console just for try"
    This reverts commit 6e124ed46078564b72cbcb12a1af42750c84e741.

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -22,7 +22,6 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-  console.log('action', action)
   switch (action.type) {
     case 'VOTE': {
       const id = action.payload.id
